8f7436d68d4d3181aa72f54195e05b4c91bfe2e0
EaswerA <easwer567@gmail.com>
2025-08-23 08:14:09 +0530

    we did in cir class mannnnn
    i got cooked in starathon :((((

--- medium/wordvalidaftersubs.java
+++ medium/wordvalidaftersubs.java
@@ -1,5 +1,14 @@
-class Solution{
-  public boolean isValid(String s){
-    
-  }
+class Solution {
+    public boolean isValid(String s) {
+        int i = 0;
+        char[] chararr = s.toCharArray();
+        for(int j=0;j<s.length();j++){
+            chararr[i]=chararr[j];
+            i++;
+            if(i>=3 && chararr[i-3]=='a' && chararr[i-2]=='b' && chararr[i-1]=='c'){
+                i-=3;
+            }
+        }
+        return i==0;
+    }
 }
