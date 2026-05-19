e0c495b3b68ac3b03ff1cdf7849a92e673b0d005
EaswerA <easwer567@gmail.com>
2026-05-19 13:25:48 +0530

    i dont evn play sudoku lol

---
+++ medium/validsudoku.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] r = new boolean[9][9];
+        boolean[][] c = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxInd = (i / 3) * 3 + (j / 3);
+                    if (r[i][num] || c[j][num] || box[boxInd][num]) {
+                        return false;
+                    }
+                    r[i][num] = c[j][num] = box[boxInd][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
